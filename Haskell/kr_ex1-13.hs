1eb3fdb6c92f3a4b31610ea3c3b10d95bb336eb3
Kaleb King <kjkwriting3@gmail.com>
2026-05-06 06:04:56 -0400

    Add Haskell translation of K&R ex.1-13

--- Haskell/kr_ex1-13.hs
+++ Haskell/kr_ex1-13.hs
@@ -1,7 +1,14 @@
-import Data.List (sort, group)
+import Data.Char (isAlpha)
+import Data.List (group, sort)
 
 main :: IO ()
-main = interact histogram
-
-histogram :: String -> String
-histogram = undefined --For now, but this is where my code is supposed to go.
+main = interact $ \contents ->
+    let ws       = map (filter isAlpha) (words contents)
+        cleaned  = filter (not . null) ws
+        lengths  = map length cleaned
+        grouped  = [(x, length xs) | xs@(x:_) <- group (sort lengths)] 
+        maxWidth = length (show(fst (last grouped)))
+        pad len  = let s = show len
+                   in replicate (maxWidth - length s) ' ' ++ s
+    in '\n' : unlines [ pad len ++ " | " ++ replicate count '#' | (len, count) <- grouped]
+    
